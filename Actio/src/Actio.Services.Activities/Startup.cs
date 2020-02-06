ae584ecae4e51cf4395132230ee8b096cab67ebe
AgostonAttila <atesz014@gmail.com>
2020-02-06 16:11:55 +0100

    end of section 3 Mongodb  activities

--- Actio/src/Actio.Services.Activities/Startup.cs
+++ Actio/src/Actio.Services.Activities/Startup.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Actio.Common.Commands;
 using Actio.Common.Events;
+using Actio.Common.Mongo;
 using Actio.Common.RabbitMq;
+using Actio.Services.Activities.Domain.Repositories;
 using Actio.Services.Activities.Handlers;
+using Actio.Services.Activities.Repositories;
+using Actio.Services.Activities.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,29 +34,41 @@ namespace Actio.Services.Activities
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddLogging();
+            services.AddMongoDB(Configuration);
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IActivityRepository, ActivityRepository>();
+            services.AddScoped<IDatabaseSeeder, CustomMongoSeeder>();
             services.AddRabbitMq(Configuration);
             services.AddScoped<ICommandHandler<CreateActivity>, CreateActivityHandler>();
+            services.AddScoped<IActivityService, ActivityService>();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            /*  if (env.IsDevelopment())
+                      {
+                          app.UseDeveloperExceptionPage();
+                      }
+
+                      app.UseHttpsRedirection();
+
+                      app.UseRouting();
+
+                      app.UseAuthorization();
+
+                      app.UseEndpoints(endpoints =>
+                      {
+                          endpoints.MapControllers();
+                      });*/
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.ApplicationServices.GetService<IDatabaseInitializer>().InitializeAsync();
+            app.UseMvc();
         }
     }
 }
